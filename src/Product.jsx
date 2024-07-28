6c8059710dc71d89a08c7d8fd410204f64aa38d5
Rishi Sharma <rishisharma222002@gmail.com>
2024-07-29 01:43:48 +0530

    use of condition via terniary operator

--- src/Product.jsx
+++ src/Product.jsx
@@ -2,23 +2,13 @@ import "./Product.css";
 
 function Product({title, price}) {
   // first change we will made at 
-  if(price>30000){
     return (
     <div className="product">
       <h3>{title}</h3>
       <h5>Price is : {price}</h5>
-      <p>Discount of 5%</p>
+      {price > 30000 ? <p>Discount of 5%</p> : null}
     </div>
   );
-  }else{
-    return(
-      <div className="product">
-        <h3>{title}</h3>
-        <h5>Price is : {price}</h5>
-      </div>
-    );
-  }
-  
 }
 
 export default Product;
