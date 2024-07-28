f3fe0a2afebd2bb2ca191b111c1ee3cebdf6a328
Rishi Sharma <rishisharma222002@gmail.com>
2024-07-29 01:17:02 +0530

    use of rendring array by maps

--- src/Product.jsx
+++ src/Product.jsx
@@ -1,13 +1,14 @@
 import "./Product.css";
 
 function Product({title, price, features,features2}) {
-  console.log(features);
+  const list = features.map((features)=><li>{features}</li>);
+  // console.log(features);
   return (
     <div className="product">
       <h3>{title}</h3>
       <h5>Price is : {price}</h5>
       {/* print features arrays */}
-      <p>{features}</p>
+      <p>{list}</p>
       <p>{features2.a}</p> 
     </div>
   );
